ffffde48e9b4f4a3e29e839653a07c847d202b26
satishchaubey <satishchaubey02@gmail.com>
2025-04-18 01:04:18 +0530

    added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SmoothScroll from '@/components/SmoothScroll'
 import Header from "@/components/Header";
